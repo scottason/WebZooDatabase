1027d0baa7ddd2a86c6b024821ec52b0a3f619f8
scottason <49214076+scottason@users.noreply.github.com>
2020-02-20 18:37:09 -0600

    First Patch

---
+++ ZooAddAnimals/src/controller/AddItemServlet.java
@@ -0,0 +1,46 @@
+package controller;
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import model.Animals;
+
+/**
+ * Servlet implementation class AddItemServlet
+ */
+@WebServlet("/addItemServlet")
+public class AddItemServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddItemServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String store = request.getParameter("store");
+		String item = request.getParameter("item");
+		
+		
+		Animals li = new Animals(store, item);
+		ListItemHelper dao =new ListItemHelper();
+		dao.insertItem(li);
+		getServletContext().getRequestDispatcher("/index.html").forward(request, response);
+	}
+
+}
---
+++ ZooAddAnimals/src/controller/EditItemServlet.java
@@ -0,0 +1,52 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Animals;
+
+/**
+ * Servlet implementation class EditItemServlet
+ */
+@WebServlet("/editItemServlet")
+public class EditItemServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditItemServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		ListItemHelper dao = new ListItemHelper();
+		String store = request.getParameter("store");
+		String item = request.getParameter("item");
+		Integer tempId = Integer.parseInt(request.getParameter("id"));
+		Animals itemToUpdate = dao.searchForAnimalById(tempId);
+		itemToUpdate.setAmount(item);
+		itemToUpdate.setSpecies(store);
+		dao.updateItem(itemToUpdate);
+		getServletContext().getRequestDispatcher("/viewAllItemsServlet").forward(request, response);
+	}
+
+}
